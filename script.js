6b7b2179a0df5ca325256cff11bb85428a72557f
Pedro Teixeira <pedrotvsantos@rede.ulbra.br>
2021-07-07 15:59:31 -0300

    update images hidden
    agora as imagens somem

--- script.js
+++ script.js
@@ -19,6 +19,55 @@
         document.getElementById("mudarimg2").src = "https://cdn.discordapp.com/attachments/390326672853041154/862056620459556916/himikomagica2.png";
         document.getElementById("mudarimg3").src = "https://cdn.discordapp.com/attachments/390326672853041154/862056714990518302/rouama.png";
     }
+
+    function esconderimg1() {
+        document.getElementById("escondeesconde1").style.visibility = "hidden";
+    }
+
+    function esconderimg2() {
+        document.getElementById("escondeesconde2").style.visibility = "hidden";
+    }
+
+    function esconderimg3() {
+        document.getElementById("escondeesconde3").style.visibility = "hidden";
+    }
+        
+    function esconderimg4() {
+        document.getElementById("escondeesconde4").style.visibility = "hidden";
+    }
+
+    function esconderimg5() {
+        document.getElementById("escondeesconde5").style.visibility = "hidden";
+    }
+
+    function esconderimg6() {
+        document.getElementById("escondeesconde6").style.visibility = "hidden";
+    }
+
+    function esconderimg7() {
+        document.getElementById("escondeesconde7").style.visibility = "hidden";
+    }
+
+    function esconderimg8() {
+        document.getElementById("escondeesconde8").style.visibility = "hidden";
+    }
+
+    function esconderimg9() {
+        document.getElementById("escondeesconde9").style.visibility = "hidden";
+    }
+
+    function esconderimg0() {
+        document.getElementById("escondeesconde0").style.visibility = "hidden";
+    }
+
+    function esconderimg11() {
+        document.getElementById("escondeesconde11").style.visibility = "hidden";
+    }
+    
+    function esconderimg12() {
+        document.getElementById("escondeesconde12").style.visibility = "hidden";
+    }
+
     function alerta() {
         alert("Obrigado por acessar!");
-      }
+    }
